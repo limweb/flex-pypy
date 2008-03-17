459f3ee0027ee3977cf93bb4ed20a815efba14d5
ricardoquesada <ricardoquesada@dbd81ab4-9648-0410-a770-9b81666e587d>
2008-03-17 15:51:19 +0000

    added native trace function

--- flex-backend/pypy/translator/flex/modules/flex.py
+++ flex-backend/pypy/translator/flex/modules/flex.py
@@ -4,5 +4,10 @@ from pypy.rpython.ootypesystem.bltregistry import BasicExternal, MethodDesc
 
 def flexTrace(s):
     pass
+
+def trace(s):
+    pass
+
 register_external(flexTrace, args=[str], export_name="_consts_0.flexTrace")
+register_external(trace, args=[str], export_name="trace")
 
