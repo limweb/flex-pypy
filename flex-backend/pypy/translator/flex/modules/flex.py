3dbd7a436e72ce58f85db75452fc7f3ef3eeae45
lucio.torre <lucio.torre@dbd81ab4-9648-0410-a770-9b81666e587d>
2008-03-17 17:54:53 +0000

    sample class in flex.py

--- flex-backend/pypy/translator/flex/modules/flex.py
+++ flex-backend/pypy/translator/flex/modules/flex.py
@@ -4,10 +4,26 @@ from pypy.rpython.ootypesystem.bltregistry import BasicExternal, MethodDesc
 
 def flexTrace(s):
     pass
+register_external(flexTrace, args=[str], export_name="_consts_0.flexTrace")
 
 def trace(s):
     pass
-
-register_external(flexTrace, args=[str], export_name="_consts_0.flexTrace")
 register_external(trace, args=[str], export_name="trace")
+
+def addChild(what):
+    pass
+register_external(addChild, args=None, export_name="addChild")
+    
+    
+class Button(BasicExternal):
+    _render_name = "Button"
+    _render_class = "Button"
+    _fields = {
+        'x': int,
+        'y': int,
+    }
+
+    _methods = {
+        'move': MethodDesc([int, int]),
+    }
 
