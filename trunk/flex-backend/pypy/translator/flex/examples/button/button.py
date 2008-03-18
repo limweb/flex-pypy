aa66b9f5d43950be8c464bedb823b72e09e0b159
lucio.torre <lucio.torre@dbd81ab4-9648-0410-a770-9b81666e587d>
2008-03-18 00:37:57 +0000

    calllbacks

--- trunk/flex-backend/pypy/translator/flex/examples/button/button.py
+++ trunk/flex-backend/pypy/translator/flex/examples/button/button.py
@@ -1,13 +1,20 @@
 from pypy.translator.flex.modules.flex import *
 from pypy.rpython.ootypesystem.bltregistry import BasicExternal, MethodDesc
 
+    
+def callback(event):
+    flexTrace("hola")        
+   
+    
+        
 def flash_main( x=1 ):
-
+    window = castToWindow( x ) 
     b = Button()
+    b.addEventListener( "click" , callback)
     b.label = "Hello world!"
     b.x=20
     b.y=52
 
-    w = castToWindow( x )
-    w.addChild(b)
-
+    window.addChild(b)
+    
+    
